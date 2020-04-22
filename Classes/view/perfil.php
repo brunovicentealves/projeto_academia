657f6e2c4e2c9e4ebcb0d5b093bb991c49f05fdc
Bruno Alves <brunovicentealves.ti@gmail.com>
2020-04-22 16:53:19 -0300

    implementado layout alterar senha

--- Classes/view/perfil.php
+++ Classes/view/perfil.php
@@ -22,7 +22,7 @@ require_once '../Controller/ControllerPerfil.php';
     <div class="form-row">
         <div class="form-group col-md-1">
             <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" style="background-color: #FF7F50;" class="btn btn-secondary">Alterar Senha</button>
+                <button type="button" style="background-color: #FF7F50;" class="btn btn-secondary"  data-toggle="modal" data-target="#exampleModal">Alterar Senha</button>
                 <button type="button" style="background-color: #FF7F50;"class="btn btn-secondary">Atualizar Cadastros</button>
             </div>
         </div>
@@ -80,6 +80,35 @@ require_once '../Controller/ControllerPerfil.php';
 </div>
 
 <?php include('../view/footer.php') ?>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Alterar Senha</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="row">
+                        <div class="col">
+                            <input type="password" class="form-control" placeholder=" Nova Senha">
+                        </div>
+                        <div class="col">
+                            <input type="password" class="form-control" placeholder="Confirmar Nova senha ">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+
+                <button type="button" style="background-color: #FF7F50;"  class="btn btn-primary">Alterar</button>
+            </div>
+        </div>
+    </div>
+</div>
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
