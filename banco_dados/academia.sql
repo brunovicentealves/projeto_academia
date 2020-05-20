a6368f906b284ce0979ae7d1e3952dcb3319d5bf
Bruno Alves <brunovicentealves.ti@gmail.com>
2020-05-20 17:03:00 -0300

    foi realizado o alterações no banco

--- banco_dados/academia.sql
+++ banco_dados/academia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 15-Maio-2020 às 19:10
+-- Generation Time: 20-Maio-2020 às 21:59
 -- Versão do servidor: 10.1.38-MariaDB
 -- versão do PHP: 7.3.4
 
@@ -23,10 +23,7 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
--- ------------
---  NOME BANCO
--- ------------
-CREATE DATABASE academia;
+
 --
 -- Estrutura da tabela `alimento`
 --
@@ -35,8 +32,17 @@ CREATE TABLE `alimento` (
   `id_alimento` int(11) NOT NULL,
   `alimento` varchar(45) DEFAULT NULL,
   `quantidade` varchar(45) DEFAULT NULL,
+  `horario_refeicao` varchar(30) DEFAULT NULL,
   `id_refeicao` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `alimento`
+--
+
+INSERT INTO `alimento` (`id_alimento`, `alimento`, `quantidade`, `horario_refeicao`, `id_refeicao`) VALUES
+(2, 'brocolis', '12', '12:30', 11),
+(3, 'arroz', '2', '34:53', 11);
 
 -- --------------------------------------------------------
 
@@ -71,11 +77,7 @@ CREATE TABLE `avaliacao` (
 --
 
 INSERT INTO `avaliacao` (`id_avaliacao`, `his_clinico`, `peso`, `altura`, `imc`, `torax`, `cintura`, `abdomen`, `quadril`, `antebraco_esquerdo`, `antebraco_direito`, `braco_esquerdo`, `braco_direito`, `coxa_esquerdo`, `coxa_direito`, `panturrilha_esquerda`, `panturrilha_direita`, `data_avaliacao`, `id_ficha`) VALUES
-(38, 'sdas', 90, 1.81, 27, 11.1, 11.1, 11.1, 11.11, 11.1, 11.1, 11.1, 11, 11, 11.1, 11.1, 11.1, '2020-05-04', 8),
-(54, 'dfs', 23.42, 2.34, 4, 23.4, 23, 23.4, 23, 34, 23.4, 23.4, 34.2, 23.4, 23, 34, 34, '2020-05-06', 8),
-(57, 'emerson viadinho', 53.53, 5.34, 1, 34.53, 35.43, 53.45, 35.43, 35.34, 35.34, 34.53, 35.34, 34.53, 34.53, 35.34, 34.53, '2020-05-08', 1),
-(60, 'ete', 23.42, 5.34, 0, 34.53, 34.53, 34.53, 34.53, 34.5, 35.3, 35.34, 35, 34.53, 34.5, 34.5, 35.3, '2020-05-06', 1),
-(72, 'teste', 11.11, 1.11, 9, 11.11, 11.11, 11.11, 11.11, 11.11, 11.11, 11.11, 11.11, 11.11, 11.11, 11.11, 11.11, '2020-05-08', 1);
+(63, 'fgbbc', 80, 1.11, 64, 32, 32.34, 23.42, 24.23, 23.4, 23.42, 23.42, 23.42, 23.4, 23.4, 23.4, 24.32, '2020-05-19', 9);
 
 -- --------------------------------------------------------
 
@@ -90,6 +92,16 @@ CREATE TABLE `exercicio` (
   `descricao_exercicio` varchar(100) DEFAULT NULL,
   `id_treino` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `exercicio`
+--
+
+INSERT INTO `exercicio` (`id_exercicio`, `nome_exercicio`, `serie`, `descricao_exercicio`, `id_treino`) VALUES
+(133, 'zcz', 'zxc', 'zxc', 42),
+(134, 'teste', 'qwq', 'wer', 42),
+(135, 'tes', 'tes', 'tes', 43),
+(136, 'extensor', '4', 'fazer para sentir o musculo ', 57);
 
 -- --------------------------------------------------------
 
@@ -108,8 +120,8 @@ CREATE TABLE `ficha` (
 --
 
 INSERT INTO `ficha` (`id_ficha`, `objetivo_ficha`, `id_usuario`) VALUES
-(1, NULL, 2),
-(8, NULL, 4);
+(9, NULL, 2),
+(10, NULL, 4);
 
 -- --------------------------------------------------------
 
@@ -139,7 +151,7 @@ CREATE TABLE `professor` (
 --
 
 INSERT INTO `professor` (`id_professor`, `nome_professor`, `sobrenome_professor`, `cref_professor`, `email_professor`, `telefone`, `senha_professor`, `endereco`, `cep`, `complemento`, `cidade`, `pais`, `estado`, `numero`) VALUES
-(1, 'bruno', 'vicente', '3442423', 'brunovicentealves.ti@gmail.com', '(43) 5.3453-534', '81dc9bdb52d04dc20036dbd8313ed055', 'rua 16', '902.000.01', 'casa', 'Porto Alegre', 'Brasil', 'Brasil', 342);
+(1, 'Bruno', 'vicente', '3442423', 'brunovicentealves.ti@gmail.com', '(43) 5.3453-534', '81dc9bdb52d04dc20036dbd8313ed055', 'rua 16', '902.000.01', 'casa', 'Porto Alegre', 'Brasil', 'Brasil', 342);
 
 -- --------------------------------------------------------
 
@@ -171,10 +183,17 @@ CREATE TABLE `refeicao` (
   `id_refeicao` int(11) NOT NULL,
   `nome_refeicao` varchar(200) DEFAULT NULL,
   `descricao_refeicao` varchar(300) DEFAULT NULL,
-  `hora_refeicao` varchar(100) DEFAULT NULL,
-  `data` date DEFAULT NULL,
+  `data_criacao` date DEFAULT NULL,
   `id_ficha` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `refeicao`
+--
+
+INSERT INTO `refeicao` (`id_refeicao`, `nome_refeicao`, `descricao_refeicao`, `data_criacao`, `id_ficha`) VALUES
+(2, 'cardapio fitnes', 'comida', '2020-11-11', 9),
+(11, 'perna', 'teste', '2020-05-20', 9);
 
 -- --------------------------------------------------------
 
@@ -186,9 +205,21 @@ CREATE TABLE `treino` (
   `id_treino` int(11) NOT NULL,
   `nome_treino` varchar(100) DEFAULT NULL,
   `descricao_treino` varchar(100) DEFAULT NULL,
-  `data` date DEFAULT NULL,
+  `data_criacao` date DEFAULT NULL,
   `id_ficha` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `treino`
+--
+
+INSERT INTO `treino` (`id_treino`, `nome_treino`, `descricao_treino`, `data_criacao`, `id_ficha`) VALUES
+(42, 'testessss', 'zxc', '2020-05-19', 9),
+(43, 'teste', 'dfdfg', '2020-05-19', 9),
+(54, 'teste', 'teste', '2020-05-19', 9),
+(55, 'test', 'testw', '2020-05-19', 9),
+(56, 'teste2', 'zxc', '2020-05-19', 9),
+(57, 'perna', 'treinos para as pernas', '2020-05-19', 10);
 
 -- --------------------------------------------------------
 
@@ -294,25 +325,25 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `alimento`
 --
 ALTER TABLE `alimento`
-  MODIFY `id_alimento` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_alimento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `avaliacao`
 --
 ALTER TABLE `avaliacao`
-  MODIFY `id_avaliacao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+  MODIFY `id_avaliacao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
 
 --
 -- AUTO_INCREMENT for table `exercicio`
 --
 ALTER TABLE `exercicio`
-  MODIFY `id_exercicio` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_exercicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=137;
 
 --
 -- AUTO_INCREMENT for table `ficha`
 --
 ALTER TABLE `ficha`
-  MODIFY `id_ficha` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_ficha` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `professor`
@@ -330,13 +361,13 @@ ALTER TABLE `professor_usuario`
 -- AUTO_INCREMENT for table `refeicao`
 --
 ALTER TABLE `refeicao`
-  MODIFY `id_refeicao` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_refeicao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `treino`
 --
 ALTER TABLE `treino`
-  MODIFY `id_treino` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_treino` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT for table `usuario`
